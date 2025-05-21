29f689c9c20f075747a88682e9ecfbdd2b0592d3
Etienne Lachaud <etienne.lachaud@ampere.cars>
2025-05-21 11:42:56 +0200

    feat: add RequestFileTransfer structs
    Add Req and Rsp structures to handle RequestFileTransfer UDS function.
    Add ModeOfOperation enum to handle properly the different types of requests.
    
    Signed-off-by: Etienne Lachaud <etienne.lachaud@ampere.cars>

--- src/proto/transfers.rs
+++ src/proto/transfers.rs
@@ -24,6 +24,65 @@ pub struct RequestDownloadRsp {
     pub max_block_size: usize,
 }
 
+#[repr(u8)]
+#[derive(Clone, Copy, Debug, Default, PartialEq)]
+/// Defines the behaviour of `RequestFileTransferReq` operation
+pub enum ModeOfOperation {
+    /// Reserved for future definition
+    #[default]
+    Reserved,
+    /// Download a file to the server
+    AddFile,
+    /// Delete a file located in the server
+    DeleteFile,
+    /// Replace a file located in the server
+    ReplaceFile,
+    /// Upload a file from the server
+    ReadFile,
+    /// Read the content of a directory from the server
+    ReadDir,
+}
+
+#[derive(Clone, Debug, Default)]
+/// Request to add/delete/replace/read a file or path (server with file system)
+pub struct RequestFileTransferReq {
+    /// Type of operation to be applied to `path_name`
+    pub mode_of_operation: ModeOfOperation,
+    /// Location on the server file system
+    pub path_name: String,
+    /// Compression method (oem specific)
+    pub compression_method: u8,
+    /// Encryption method (oem specific)
+    pub encryption_method: u8,
+    /// Size in byte for parameters `file_size_uncompressed` and `file_size_compressed`
+    pub file_size_bytes: u8,
+    /// Size in byte of the uncompressed file
+    pub file_size_uncompressed: usize,
+    /// Size in byte of the compressed file (equal to `file_size_uncompressed` if uncompressed)
+    pub file_size_compressed: usize,
+}
+
+#[derive(Clone, Debug, Default)]
+/// Response to a [`RequestFileTransferReq`]
+pub struct RequestFileTransferRsp {
+    /// Type of operation applied to the file/dir
+    pub mode_of_operation: ModeOfOperation,
+    /// Size in byte of the `max_block_size` field (ie. u8, u16, u32 ...)
+    pub max_block_size_bytes: u8,
+    /// Maximum size of data in [`TransferDataReq::data`]
+    pub max_block_size: usize,
+    /// Compression method (oem specific)
+    pub compression_method: u8,
+    /// Encryption method (oem specific)
+    pub encryption_method: u8,
+    /// Size in byte for the parameters `file_dir_size_uncompressed` and `file_size_compressed`
+    pub file_dir_data_size_bytes: u16,
+    /// Size in byte of the uncompressed file to be uploaded
+    pub file_dir_size_uncompressed: usize,
+    /// Size in byte of the compressed file
+    pub file_size_compressed: usize,
+}
+
 #[derive(Clone, Debug, Default)]
 /// Transfer data, i.e. chunk of the Download or Upload
 ///
